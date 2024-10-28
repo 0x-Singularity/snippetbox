9b56ac320fb066ac853016ff9ade3d89d5b8a316
0xSingularity <gmcguir2@kennesaw.students.edu>
2024-10-28 19:35:02 -0400

    on page 27

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
   "log"
   "net/http"
+  "strconv"
+  "fmt"
 )
 
   // Define a home handler function which writes a byte slice containing
@@ -11,14 +13,33 @@ import (
     w.Write([]byte("Hello from Snippetbox"))
   }
 
+  func snippetView(w http.ResponseWriter, r *http.Request) {
+    id, err := strconv.Atoi(r.PathValue("id"))
+    if err != nil || id < 1{
+      http.NotFound(w,r)
+      return
+    }
+
+    msg := fmt.Sprintf("Display a specific snippet with ID %d..", id)
+    w.Write([]byte(msg))
+  }
+
+  func snippetCreate(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Display a form for creating a new snippet"))
+  }
+
+
+
   func main() {
     // Use the http.NewServeMux() function to initialize a new servemux, then
     // register the home function as the handler for the "/" URL pattern.
     mux := http.NewServeMux()
-    mux.HandleFunc("/",home)
+    mux.HandleFunc("/{$}",home)
+    mux.HandleFunc("/snippet/view/{id}",snippetView)
+    mux.HandleFunc("/snippet/create", snippetCreate)
 
     // Print a log message to say that the server is starting
-    log.Print("starting on server 4000")
+    log.Print("starting server on port 4000")
 
     err := http.ListenAndServe(":4000", mux)
     log.Fatal(err)
